c856a1dcb8d699ab09391a0928f8befadb23e1fe
AhmedJaved1 <127095065+AhmedJaved1@users.noreply.github.com>
2025-05-07 13:43:47 +0500

    Create Calendar for Due Dates.js

---
+++ Calendar for Due Dates.js
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flatpickr/4.6.9/flatpickr.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flatpickr/4.6.9/flatpickr.min.js"></script>
+
+<script>
+    flatpickr("#dueDate", {
+        minDate: "today",
+        dateFormat: "Y-m-d",
+        allowInput: true
+    });
+</script>
